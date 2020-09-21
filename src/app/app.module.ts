671026ac3b204e22411f60f7a48e0d04561403ab
yunweneric <yunweneric@gmail.com>
2020-09-21 17:07:29 +0100

    added footer and header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { LoginComponent } from './users/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
